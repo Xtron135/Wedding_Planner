4a9e036c77f8e6db232ba2d374932b1c6bd8cc77
Xtron135 <94394142+Xtron135@users.noreply.github.com>
2026-08-11 15:51:46 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 // TUKAR dua baris ni ikut GitHub username & nama repo korang
 export const GITHUB_OWNER = "Xtron135";
-export const GITHUB_REPO = "wedding-planner";
+export const GITHUB_REPO = "Wedding_Planner";
 export const GITHUB_BRANCH = "main";
 export const DATA_PATH = "data";
